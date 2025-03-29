af1c8b8c49df459eee9622cd2451ddbc90217cdf
Johan Valencia <johanmejiaxlarchaval@gmail.com>
2025-03-29 17:41:05 -0500

    Agregando el proyecto a la nueva rama

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const cors = require("cors");
-const connectDB = require("./config/db"); 
+const cors = require("cors"); // Importa CORS
+const connectDB = require("./config/db"); // Asegúrate de que esta conexión esté configurada correctamente
 const authRoutes = require("./routes/authRoutes");
-const productRoutes = require("./routes/productRoutes");
+const salesRoutes = require("./routes/salesRoutes");
 
 dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json({ limit: '50mb' })); // Aumentamos el límite para manejar imágenes base64
-
-
+app.use(express.json());
 
 // 🟢 Habilitar CORS para permitir peticiones desde el frontend
 app.use(cors({
-<<<<<<< HEAD
-    origin: ["http://127.0.0.1:5501", "http://localhost:5501"],
+    origin: "https://programa-losioines.netlify.app", // Ajusta según la URL de tu frontend
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
-=======
-    origin: ['http://127.0.0.1:5501', 'https://programa-losioines.netlify.app'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }));
->>>>>>> 2b2c546 (configuracion del cors para localhost)
 
 // Rutas
 app.use("/api/auth", authRoutes);
-app.use("/api/products", productRoutes); // Cambiamos a 'products' para más claridad
+app.use("/api/sales", salesRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
