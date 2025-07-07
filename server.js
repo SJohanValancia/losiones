a40f2b902812b845d8e3a252d9b6936304578696
Johan Valencia <johanmejiaxlarchaval@gmail.com>
2025-07-07 07:40:32 -0500

    demora mucho y no abre 2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 // 🟢 Habilitar CORS para permitir peticiones desde el frontend
 app.use(cors({
-    origin: ["https://programa-losioines.netlify.app", "http://127.0.0.1:5502"], // Ajusta según la URL de tu frontend
+    origin: ["https://programa-losioines.netlify.app", "http://127.0.0.1:5502", "http://127.0.0.1:5503"], // Ajusta según la URL de tu frontend
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH "],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
